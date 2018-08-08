ff2c5a56ad1c4e2b88ab7f8329eb13bd6dd6a0a5
dabuu <dabuu@outlook.com>
2018-08-08 23:48:41 +0800

    finish chap1.1

--- chap1/1.1.py
+++ chap1/1.1.py
@@ -30,17 +30,41 @@ class FrenchDeck:
     def get_same_ranks(self, rank_name):
         if rank_name in self.ranks:
             return self[self.ranks.index(rank_name)::13]
+fd = FrenchDeck()
+
+def test_len():
+    print len(fd)
+
+def test_getitem():
+    print "# =========== slice"
+    print fd[10]
+    print fd[-2]
+
+    print fd[:3]
+    print fd[-1:]
+    print fd[12::13]
+
+    print "# =========== choice random"
+    for x in range(5):
+        print choice(fd)
+
+    print "# =========== iter & sort"
+    # for card in fd:
+    #     print card
+    # for card in reversed(fd):
+    #     print card
+
+    for card in sorted(fd, key=spades_high):
+        print card
+
+suits_value = dict(spades=3, hearts=2, diamonds=1,clubs=0)
+def spades_high(card):
+    rank_value = FrenchDeck.ranks.index(card.rank)
+    return rank_value * len(suits_value) + suits_value[card.suit]
 
 
 if __name__ == '__main__':
-    fd = FrenchDeck()
-    print len(fd)
+    test_len()
+    test_getitem()
 
-    # =========== choice random
-    for x in range(5):
-        print fd.choice_random_card()
 
-    # =========== choice random
-    print fd.get_same_ranks("A")
-    print fd.get_same_ranks("2")
-    print fd.get_same_ranks("a")
