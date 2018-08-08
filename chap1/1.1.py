7c238cd63bd4284bfa4ba571db4358a554776d29
dabuu <dabuu@outlook.com>
2018-08-08 11:53:29 +0800

    add methods

--- chap1/1.1.py
+++ chap1/1.1.py
@@ -6,15 +6,17 @@
 """
 
 import collections
+from random import choice
 
-Card = collections.namedtuple('Card', ['rank','suit'])
+Card = collections.namedtuple('Card', ['rank', 'suit'])
+
 
 class FrenchDeck:
-    ranks = [str(n) for n in range(2,11)]+list("JQKA")
+    ranks = [str(n) for n in range(2, 11)] + list("JQKA")
     suits = "spades hearts diamonds clubs".split()
 
     def __init__(self):
-        self.cards = [Card(rank,suit) for suit in self.suits for rank in self.ranks]
+        self.cards = [Card(rank, suit) for suit in self.suits for rank in self.ranks]
 
     def __len__(self):
         return len(self.cards)
@@ -22,6 +24,23 @@ class FrenchDeck:
     def __getitem__(self, position):
         return self.cards[position]
 
+    def choice_random_card(self):
+        return choice(self.cards)
+
+    def get_same_ranks(self, rank_name):
+        if rank_name in self.ranks:
+            return self[self.ranks.index(rank_name)::13]
+
+
 if __name__ == '__main__':
     fd = FrenchDeck()
-    print fd.__len__()
+    print len(fd)
+
+    # =========== choice random
+    for x in range(5):
+        print fd.choice_random_card()
+
+    # =========== choice random
+    print fd.get_same_ranks("A")
+    print fd.get_same_ranks("2")
+    print fd.get_same_ranks("a")
