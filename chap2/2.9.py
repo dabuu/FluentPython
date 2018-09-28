377df7a9968cbaf7cbbc858fdc59a8699caaa4b5
dabuu <dabuu@outlook.com>
2018-09-28 17:27:57 +0800

    add 2.9

---
+++ chap2/2.9.py
@@ -0,0 +1,13 @@
+# coding:utf-8
+"""
+@file: 2.9.py
+@time: 2018/9/24 下午8:32
+@contact: dabuwang
+@desc: when "list" is NOT first choice
+"""
+
+def array_test_1():
+    pass
+
+if __name__ == '__main__':
+    array_test_1()
