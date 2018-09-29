78eabc70de8bb0fb8c7db1db7ef2e8fbaa895743
dabuwang <dabuwang@tencent.com>
2018-09-29 16:41:18 +0800

    add

--- chap2/2.9.py
+++ chap2/2.9.py
@@ -6,8 +6,43 @@
 @desc: when "list" is NOT first choice
 """
 
+import tempfile
+from array import array
+from random import random
+
 def array_test_1():
-    pass
+    """
+    when the type is specific, array is better choice
+    :return:
+    """
+    floats = array('d', (random() for i in xrange(10**7)))  # 'd' means double type
+    print floats[-1]
+
+    test_temp = tempfile.mktemp(prefix="test29_")
+    with open(test_temp, 'wb') as fp:
+        floats.tofile(fp)
+
+    floats2 = array('d')
+    with open(test_temp, 'rb') as fp:
+        floats2.fromfile(fp, 10**7)
+
+    print floats2[-1]
+
+def memview_test_2():
+    numbers = array('h', [-2, -1, 0, 1, 2])
+    numbers = b'abcefg'
+    print numbers
+
+    memv = memoryview(numbers)
+    print len(memv)
+    print memv
+
+    memv_oct = memv.cast('B')
+    print memv_oct.tolist()
+    memv_oct[5] = 4
+    print numbers
+
 
 if __name__ == '__main__':
-    array_test_1()
+    # array_test_1()
+    memview_test_2()
